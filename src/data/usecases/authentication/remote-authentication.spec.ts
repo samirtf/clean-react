8f98f0ac27b03142b44dece6fbc0dae85080ac83
Samir Trajano Feitosa <samirtf@outlook.com>
2021-05-18 11:39:58 -0300

    test: ensure RemoteAuthentication returns an Account on success

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,6 @@
 import { HttpPostClientSpy } from 'data/test/mock-http-client'
 import { InvalidCredentialsError } from 'domain/errors/invalid-credentials-error'
-import { mockAuthentication } from 'domain/test/mock-authentication'
+import { mockAccountModel, mockAuthentication } from 'domain/test/mock-account'
 import { RemoteAuthentication } from './remote-authentication'
 import { HttpStatusCode } from 'data/protocols/http/http-response'
 import { AuthenticationParams } from 'domain/usecases/authentication'
@@ -71,5 +71,16 @@ describe('RemoteAuthentication', () => {
     }
     const promise = sut.auth(mockAuthentication())
     await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  test('Should return an AccountModel if HttPostClient returns 200', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const httpResult = mockAccountModel()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult
+    }
+    const account = await sut.auth(mockAuthentication())
+    await expect(account).toEqual(httpResult)
   })
 })
