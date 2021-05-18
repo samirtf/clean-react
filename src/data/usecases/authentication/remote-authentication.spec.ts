cd7ee736486382c72daaac8b69682aeefcfba83e
Samir Trajano Feitosa <samirtf@outlook.com>
2021-05-18 11:53:08 -0300

    fix: typo

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -39,7 +39,7 @@ describe('RemoteAuthentication', () => {
   test('Should throw InvalidCredentialsError if HttPostClient returns 401', async () => {
     const { sut, httpPostClientSpy } = makeSut()
     httpPostClientSpy.response = {
-      statusCode: HttpStatusCode.unanthorized
+      statusCode: HttpStatusCode.unauthorized
     }
     const promise = sut.auth(mockAuthentication())
     await expect(promise).rejects.toThrow(new InvalidCredentialsError())
