fa3ee2c622d447eb2541fa7e9302829130846ac2
Samir Trajano Feitosa <samirtf@outlook.com>
2021-10-26 01:13:09 -0300

    refactor: add index files with multiple imports

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,6 @@
-import Login from 'presentation/pages/login/login'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Login } from 'presentation/pages/'
 
 ReactDOM.render(
   <Login />,
